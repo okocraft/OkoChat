806d856118a14e8a6c17be1acf1ca65e8049b999
Siroshun09 <siro.shun8@gmail.com>
2025-06-22 19:04:52 +0900

    build(api,core): add jfun-result and codec4j

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,3 +1,4 @@
 dependencies {
     api(libs.event4j.api)
+    api(libs.jfun.result)
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
     implementation(projects.okochatApi)
+    implementation(libs.codec4j.api)
+    implementation(libs.codec4j.io.yaml)
     implementation(libs.event4j.tree)
 }
